5f7b14eb67f040684424e5faf9d37d9a8d84aca3
Alexander-Tengborg <Landervich@Gmail.com>
2019-04-24 19:39:11 +0200

    Updates

---
+++ precache-manifest.4e0182caeedb97a9e430f0a3bb4d2f4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c929600099eaaa065f58faba54e1fa50",
+    "url": "/GoT-Tracker/index.html"
+  },
+  {
+    "revision": "83a098b80666955ffefa",
+    "url": "/GoT-Tracker/static/css/main.5cab2242.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/GoT-Tracker/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "83a098b80666955ffefa",
+    "url": "/GoT-Tracker/static/js/main.028e1302.chunk.js"
+  },
+  {
+    "revision": "ee6a57a292a0b57bdd3c",
+    "url": "/GoT-Tracker/static/js/runtime~main.c07e7983.js"
+  }
+]);
